600ef86402f5fd0f0c1bcfe3bf757dafa95e9932
Reda Damene <90632620+RedaDamene@users.noreply.github.com>
2023-11-15 10:27:50 +0100

    Add users and chatbot

---
+++ app/ui/components/Product/AddToCart.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+function AddToCart() {
+    return (
+        <div>
+            <button onClick={() => console.log('Click')}>Ajouter au panier</button>
+        </div>
+    );
+}
+
+export default AddToCart;
---
+++ app/ui/components/Product/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddToCart from "@/app/ui/components/Product/AddToCart";
+
+function ProductCard() {
+    return (
+        <div>
+            <AddToCart/>
+        </div>
+    );
+}
+
+export default ProductCard;
